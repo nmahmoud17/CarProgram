63ddbcfe7a0a71c23e10eafe69c024e12494be06
Nada Mahmoud <nadamahmoud@Nadas-MacBook-Pro.local>
2019-01-22 14:08:28 -0500

    Define 3 instance variables, and created accessor methods

---
+++ src/com/company/Car.java
@@ -0,0 +1,35 @@
+package com.company;
+
+
+public class Car {
+
+    private String make;
+    private String model;
+    private String color;
+
+    public void setMake (String setMake) {
+        make = setMake;
+    }
+
+    public String getMake () {
+        return make;
+    }
+
+    public void setModel (String setModel){
+        model = setModel;
+    }
+
+    public String getModel (){
+        return model;
+    }
+
+    public void setColor (String setColor) {
+        color = setColor;
+    }
+
+    public String getColor () {
+        return color;
+    }
+
+
+}
---
+++ src/com/company/CarDemo.java
@@ -0,0 +1,17 @@
+
+package com.company;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car ();
+
+        myCar.setMake("BMW");
+        myCar.setModel("M3");
+        myCar.setColor("Black");
+
+
+
+
+    }
+}
